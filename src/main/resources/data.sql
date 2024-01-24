36a829aa8e8ffbcf282b04177f6124cd7abbb4ad
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-24 05:35:43 -0300

    Adição do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO post (id, content, author) VALUES (1, 'Primeiro valor', 'exemplo1@email.com');
+INSERT INTO post (id, content, author) VALUES (2, 'Segundo valor', 'exemplo2@email.com');
+INSERT INTO post (id, content, author) VALUES (3, 'Terceiro valor', 'exemplo3@email.com');
+INSERT INTO post (id, content, author) VALUES (4, 'Quarto valor', 'exemplo4@email.com');
+INSERT INTO post (id, content, author) VALUES (5, 'Quinto valor', 'exemplo5@email.com');

0c1d8658d5c7baf387c77368014c3da6b032b06d
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-24 03:47:30 -0300

    Initial commit

---
+++ src/test/java/br/com/vitorgabrielti/springdatapagination/SpringDataPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorgabrielti.springdatapagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

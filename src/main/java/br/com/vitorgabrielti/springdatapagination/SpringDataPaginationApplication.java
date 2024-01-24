5796443fa2fe281e3950d7ce6d7f8d1b65edd29b
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-24 05:34:55 -0300

    Aplicação pronta
    Adição do record, service e controller principal

--- src/main/java/br/com/vitorgabrielti/springdatapagination/SpringDataPaginationApplication.java
+++ src/main/java/br/com/vitorgabrielti/springdatapagination/SpringDataPaginationApplication.java
@@ -1,7 +1,16 @@
 package br.com.vitorgabrielti.springdatapagination;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringDataPaginationApplication {
@@ -10,4 +19,38 @@ public class SpringDataPaginationApplication {
 		SpringApplication.run(SpringDataPaginationApplication.class, args);
 	}
 
+	record Post(Long id, String content, String author) {
+
+	}
+
+	interface PostRepository extends PagingAndSortingRepository<Post, Long>{
+
+	}
+
+	class PostService {
+		PostRepository postRepository;
+
+		
+		public PostService(PostRepository postRepository) {
+			this.postRepository = postRepository;
+		}
+
+
+		public Page<Post> listPosts(Pageable pageable){
+			return postRepository.findAll(pageable);
+		}
+	}
+
+	@RestController
+	@RequestMapping("/posts")
+	class PostController {
+
+		@Autowired
+		PostService postService;
+
+		@GetMapping
+		public List<Post> listPosts(Pageable pageable){
+			return postService.listPosts(pageable).getContent();
+		}
+	}
 }
